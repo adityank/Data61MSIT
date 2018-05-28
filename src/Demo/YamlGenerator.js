a40934e4167fa459e01211ad9d6cad31d5552a0d
Dongliang Zhou <leoaction@gmail.com>
2018-05-28 17:49:02 -0400

    YAML generator init impl complete

--- src/Demo/YamlGenerator.js
+++ src/Demo/YamlGenerator.js
@@ -3,20 +3,110 @@ var fs = require('fs'),
     outpath = '';
 
 function generateCryptoConfig(orgs, networkName, domain) {
+    console.log('---begin generating crypto-config.yaml---');
+    var writer = fs.createWriteStream(outpath+'crypto-config.yaml');
 
+    var template = fs.readFileSync('./template/crypto-config.yaml', 'utf8');
+    // Write main body
+    template.split(/\r?\n/).forEach(function(line){
+        writer.write(eval('`'+line+'\n`'));
+    })
+    // Write peer part
+    peer_template = fs.readFileSync('./template/crypto-config-peer.yaml', 'utf8');
+    for (var i = orgs.length -1; i >= 0; i--) {
+        var peerName = orgs[i];
+        var peerDomainPrefix = peerName.toLowerCase();
+        peer_template.split(/\r?\n/).forEach(function(line){
+            writer.write(eval('`'+line+'\n`'));
+        })
+    }
+    writer.end();
+    console.log('---end generating crypto-config.yaml---');
 }
 
 function generateConfigTX(orgs, networkName, domain) {
+    console.log('---begin generating configtx.yaml---');
+    var writer = fs.createWriteStream(outpath+'configtx.yaml');
 
+    // Generate ${configtx-orgs}
+    var configtx_orgs = '';
+    var orgs_template = fs.readFileSync('./template/configtx-orgs.yaml', 'utf8');
+    for (var i = orgs.length -1; i >= 0; i--) {
+        var peerName = orgs[i];
+        orgs_template.split(/\r?\n/).forEach(function(line){
+            configtx_orgs+=eval('`'+line+'\n`');
+        });
+    }
+
+    // Write main body
+    var template = fs.readFileSync('./template/configtx.yaml', 'utf8');
+    template.split(/\r?\n/).forEach(function(line){
+        writer.write(eval('`'+line+'\n`'));
+    });
+    // Write msp part
+    msp_template = fs.readFileSync('./template/configtx-orgs-msp.yaml', 'utf8');
+    for (var i = orgs.length -1; i >= 0; i--) {
+        var peerName = orgs[i];
+        var peerDomainPrefix = peerName.toLowerCase();
+        msp_template.split(/\r?\n/).forEach(function(line){
+            writer.write(eval('`'+line+'\n`'));
+        });
+    }
+    writer.end();
+    console.log('---end generating crypto-config.yaml---');
 }
 
-function generateDockerComposeCli(orgs, networkName, domain) {
+function generateDockerComposeCli(orgs, networkName, domain) {    
+    console.log('---begin generating docker-compose-cli.yaml---');
+    var writer = fs.createWriteStream(outpath+'docker-compose-cli.yaml');
 
+    var template = fs.readFileSync('./template/docker-compose-cli.yaml', 'utf8');
+    var volumes_template = fs.readFileSync('./template/docker-compose-cli-volumes.yaml', 'utf8');
+    // Generate peerVolumes
+    var peerVolumes = '';
+    for (var i = orgs.length -1; i >= 0; i--) {
+        var peerDomainPrefix = orgs[i].toLowerCase();
+        volumes_template.split(/\r?\n/).forEach(function(line){
+            peerVolumes+=eval('`'+line+'\n`');
+        });
+    }
+    // Write main body
+    template.split(/\r?\n/).forEach(function(line){
+        writer.write(eval('`'+line+'\n`'));
+    })
+    // Write peer part
+    var peer_template = fs.readFileSync('./template/docker-compose-cli-peer.yaml', 'utf8');
+    for (var i = orgs.length -1; i >= 0; i--) {
+        var peerDomainPrefix = orgs[i].toLowerCase();
+        peer_template.split(/\r?\n/).forEach(function(line){
+            writer.write(eval('`'+line+'\n`'));
+        });
+    }    
+    // Generate cliDependsOn for cli
+    var cliDependsOn = '';
+    var dependson_template = fs.readFileSync('./template/docker-compose-cli-depends-on.yaml', 'utf8');
+    for (var i = orgs.length -1; i >= 0; i--) {
+        var peerDomainPrefix = orgs[i].toLowerCase();
+        dependson_template.split(/\r?\n/).forEach(function(line){
+            cliDependsOn+=eval('`'+line+'\n`');
+        });
+    }
+    // Write cli part
+    var cli_template = fs.readFileSync('./template/docker-compose-cli-cli.yaml', 'utf8');
+    for (var i = orgs.length -1; i >= 0; i--) {
+        var peerName = orgs[i];
+        var peerDomainPrefix = peerName.toLowerCase();
+        cli_template.split(/\r?\n/).forEach(function(line){
+            writer.write(eval('`'+line+'\n`'));
+        });
+    }
+    writer.end();
+    console.log('---end generating docker-compose-cli.yaml---');
 }
 
 function generateDockerComposeBase(orgs, networkName, domain) {
-    var writer = fs.createWriteStream(outpath+'base/docker-compose-base.yaml');
     console.log('---begin generating docker-compose-base.yaml---');
+    var writer = fs.createWriteStream(outpath+'base/docker-compose-base.yaml');
 
     var template = fs.readFileSync('./template/base/docker-compose-base.yaml', 'utf8');
     
@@ -43,8 +133,8 @@ function generateDockerComposeBase(orgs, networkName, domain) {
 }
 
 function generatePeerBase(networkName) {
-    var writer = fs.createWriteStream(outpath+'base/peer-base.yaml');
     console.log('---begin generating peer-base.yaml---');
+    var writer = fs.createWriteStream(outpath+'base/peer-base.yaml');
 
     var template = fs.readFileSync('./template/base/peer-base.yaml', 'utf8');
     
@@ -56,6 +146,7 @@ function generatePeerBase(networkName) {
 }
 
 function generateYAML(orgs, networkName, domain) {
+    console.log('---begin generating YAML files---');
     outpath = 'out/'+domain+'/'+networkName+'/';
     checkPath(domain,networkName);
     generatePeerBase(networkName);
@@ -63,6 +154,7 @@ function generateYAML(orgs, networkName, domain) {
     generateCryptoConfig(orgs, networkName, domain);
     generateConfigTX(orgs, networkName, domain);
     generateDockerComposeCli(orgs, networkName, domain);
+    console.log('---end generating YAML files---');
 }
 
 function checkPath(domain,networkName) {
