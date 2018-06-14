e9b553d1347069ab7dfbce515b769cabe25c9595
by700git <by700gm@gmail.com>
2018-06-14 15:13:29 -0400

    add gitignore

--- Parser/parse2.js
+++
@@ -1,7 +0,0 @@
-const xml = require("xml-parse");
-var fs = require('fs');
-// Valid XML string
-var file = fs.readFileSync('bpmn/pizza.bpmn', 'utf8');
-var parsedXML = xml.parse(file);
-console.log(parsedXML);
- 
