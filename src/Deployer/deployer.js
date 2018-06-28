9c30521839be776c4548bc9875318d20f2fa98a4
Aditya Kamble <akamble@andrew.cmu.edu>
2018-06-28 17:23:38 -0400

    initial commit

---
+++ src/Deployer/deployer.js
@@ -0,0 +1,14 @@
+/******************************************************************************************************************
+* File:deployer.js
+* Project: MSIT-SE Studio Project (Data61)
+* Copyright: Team Unchained
+* Versions:
+*   
+*   28 June 2018 - Aditya Kamble - Created a new file with
+*
+* Description: This is script that will deploy the network defined by the generated files in ../../out folder by the generators(translator) 
+*
+* External Dependencies: 
+* 1. files in ../../out directory
+*
+******************************************************************************************************************/
