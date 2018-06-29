61622706cfbad327bafd24e5f1bec713c18a30a4
Aditya Kamble <akamble@andrew.cmu.edu>
2018-06-29 17:52:50 -0400

    Fixed some of the logging issues
    still problems exist (order of logs)

--- src/Deployer/deployer.js
+++ src/Deployer/deployer.js
@@ -4,7 +4,8 @@
 * Copyright: Team Unchained
 * Versions:
 *   
-*   28 June 2018 - Aditya Kamble - Created a new file with
+*   28 June 2018 - Aditya Kamble - Created a new file with all commands
+* 	29 june 2018 - Aditya Kamble - Added logging and repaired the relative folder structure
 *
 * Description: This is script that will deploy the network defined by the generated files in ../../out folder by the generators(translator) 
 *
@@ -18,86 +19,189 @@ var shell = require('shelljs');
 
 var logger = require('../Logger/logger')
 
-var deploy_writer;
 
 function createJoinChannel(peer,networkName,orgDomain,first){
 
 	channelName = networkName + "channel";
 
-	if(first)
-		shell.exec("docker exec" + peer + "_cli bash /bin/sh -c 'peer channel create -o orderer." + orgDomain + ":7050 -c " + channelName + " -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/" + orgDomain +"/orderers/orderer." + orgDomain +"/msp/tlscacerts/tlsca." + orgDomain + "-cert.pem'");
-	else
-		shell.exec("docker exec" + peer + "_cli bash /bin/sh -c 'peer channel fetch newest ./" + networkName + "channel.block -o orderer." + orgDomain + ":7050 -c " + channelName + " --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/" + orgDomain + "/orderers/orderer." + orgDomain + "/msp/tlscacerts/tlsca." + orgDomain + "-cert.pem'");
+	if(first == true){
+		temp = shell.exec("docker exec" + peer + "_cli bash /bin/sh -c 'peer channel create -o orderer." + orgDomain + ":7050 -c " + channelName + " -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/" + orgDomain +"/orderers/orderer." + orgDomain +"/msp/tlscacerts/tlsca." + orgDomain + "-cert.pem'", (err, stdout, stderr) => {
+	    if (err) {
+	        // node couldn't execute the command
+	        console.log('Channel creation failed!')
+	        logger.log('deployer','Channel creation failed!');
+	        logger.log('deployer',stderr);
+	        return false;
+	    }
+	    logger.log('deployer','=================Channel created successfully!================');
+	    return true;
+	    });		
+		if(temp == false)
+			return false;
+	}
+	else{
+		temp = shell.exec("docker exec" + peer + "_cli bash /bin/sh -c 'peer channel fetch newest ./" + networkName + "channel.block -o orderer." + orgDomain + ":7050 -c " + channelName + " --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/" + orgDomain + "/orderers/orderer." + orgDomain + "/msp/tlscacerts/tlsca." + orgDomain + "-cert.pem'", (err, stdout, stderr) => {
+		    if (err) {
+		        // node couldn't execute the command
+		        console.log("Channel fetching failed for " + peer + "!")
+		        logger.log('deployer',"Channel fetching failed for " + peer + "!");
+		        logger.log('deployer',stderr);
+		        return false;
+		    }
+		    logger.log('deployer', '================' + peer + ' fetched channel successfully!=================');
+		    return true;
+		});
+	}
+	if(temp == false)
+		return false;
 
-	shell.exec("docker exec" + peer + "_cli bash /bin/sh -c 'peer channel join -b "+ networkName + "channel.block'");
+	temp = shell.exec("docker exec" + peer + "_cli bash /bin/sh -c 'peer channel join -b "+ networkName + "channel.block'", (err, stdout, stderr) => {
+	    if (err) {
+	        // node couldn't execute the command
+	        console.log("Channel joining failed for " + peer + "!")
+	        logger.log('deployer',"Channel joining failed for " + peer + "!");
+	        logger.log('deployer',stderr);
+	        return false;
+	    }
+	    logger.log('deployer', '===============' + peer + ' joined channel successfully!===============');
+	    return true;
+	});
+
+	if(temp == false)
+		return false;	
+
+	return true;
 
 }
 
 function setupNetwork(networkName,orgDomain,peers){
-	
+	console.log("In setup network");
 	shell.mkdir ('channel-artifacts');
 
 	shell.cp( fabricSamplesPath + "first-network/.env", "./.env");
 
-	shell.exec(fabricSamplesPath + "bin/cryptogen  generate --config=./crypto-config.yaml");
-	logger.log(deploy_writer,"==================\n Artifacts generated \n ==========================\n");
+	temp = shell.exec(fabricSamplesPath + "bin/cryptogen  generate --config=./crypto-config.yaml", (err, stdout, stderr) => {
+	    if (err) {
+	        // node couldn't execute the command
+	        console.log("Artifacts generation failed!")
+	        logger.log('deployer',"Artifacts generation failed!");
+	        logger.log('deployer',stderr);
+	        return false;
+	    }
+	    logger.log('deployer', '=============Artifacts generated successfully!============');
+	    return true;
+	});
+	if(temp == false)
+		return false;
 
 	shell.exec("export FABRIC_CFG_PATH=$PWD");
 
-	shell.exec(fabricSamplesPath + "bin/configtxgen -profile " + networkName + "Genesis -outputBlock ./channel-artifacts/genesis.block");
+	temp = shell.exec(fabricSamplesPath + "bin/configtxgen -profile " + networkName + "Genesis -outputBlock ./channel-artifacts/genesis.block", (err, stdout, stderr) => {
+	    if (err) {
+	        // node couldn't execute the command
+	        console.log("Genesis generation failed!")
+	        logger.log('deployer',"Genesis generation failed!");
+	        logger.log('deployer',stderr);
+	        return false;
+	    }
+	    logger.log('deployer', "==================  Genesis block created!!  ========================== ");
+	});
 
-	logger.log(deploy_writer,"==================\n Genesis block created!!\n ==========================\n");
+	if(temp == false)
+		return false;
 
-	shell.exec("export CHANNEL_NAME=" + networkName + "channel  && " + fabricSamplesPath + "bin/configtxgen -profile " + networkName + "Channel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME");
+	temp = shell.exec("export CHANNEL_NAME=" + networkName + "channel  && " + fabricSamplesPath + "bin/configtxgen -profile " + networkName + "Channel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME", (err, stdout, stderr) => {
+	    if (err) {
+	        // node couldn't execute the command
+	        console.log("Channel.tx generation failed!")
+	        logger.log('deployer',"Channel.tx generation failed!");
+	        logger.log('deployer',stderr);
+	        return false;
+	    }
+	    logger.log('deployer', "==================  Channel.tx generated!!  ========================== ");
+	});
 
-	logger.log(deploy_writer,"==================\n \n ==========================\n");
+	if(temp == false)
+		return false;
 
-	for (var peer in peers){
-		shell.exec(fabricSamplesPath + "bin/configtxgen -profile " + networkName + "channel -outputAnchorPeersUpdate ./channel-artifacts/" + peer +  "MSPanchors.tx -channelID $CHANNEL_NAME -asOrg " + peer + "MSP");
+	for(var iter=0; iter<peers.length; iter++){
+		temp = shell.exec(fabricSamplesPath + "bin/configtxgen -profile " + networkName + "channel -outputAnchorPeersUpdate ./channel-artifacts/" + peers[iter] +  "MSPanchors.tx -channelID $CHANNEL_NAME -asOrg " + peers[iter] + "MSP", (err, stdout, stderr) => {
+		    if (err) {
+		        // node couldn't execute the command
+		        console.log(peers[iter] + " generation failed!")
+		        logger.log('deployer',peers[iter] + " generation failed!");
+		        logger.log('deployer',stderr);
+		        return false;
+		    }
+		    logger.log('deployer', "==================  " + peers[iter] + " created!!  ======================= ");
+		    return true;
+		});
+		if(temp == false)
+			return false;
 	}
-
-	shell.exec("docker-compose -f docker-compose-cli.yaml up");
 
 }
 
 
-
-function deploy(networkName,orgDomain,peers){
+function deploy(networkName,orgDomain,peers,status){
 
 	outPath = "../../out/" + orgDomain + "/" + networkName + "/";
 	fabricSamplesPath = "../../../../../fabric-samples/";
 
-	deploy_writer = logger.getWriter('deployer',orgDomain,networkName);
+	logger.init(orgDomain,networkName);
+	
+	shell.cd(outPath);
+	var res = true;
+	logger.log('deployer',"........----------------Starting to log deployment-----------------.............");	
 
 	if (!shell.which('docker')) {
 		shell.echo('Sorry, this script requires docker');
-		logger.log(deploy_writer,"Tried deploying without installing docker\n");
+		logger.log('deployer',"Tried deploying without installing docker ");
 		shell.exit(1);
+		return false;
 	}
 
 	if (!shell.which('docker-compose')) {
 		shell.echo('Sorry, this script requires docker-compose');
-		logger.log(deploy_writer,"Tried deploying without installing docker-compose\n");		
+		logger.log('deployer',"Tried deploying without installing docker-compose ");		
 		shell.exit(1);
+		return false;
 	}
-
-	shell.cd(outPath);
 
 	// Setup the infrastructure and bring up the network
 
+	if(status == "new"){		
+		logger.log('deployer',"==================  Starting to setup infra   ==========================");
+		res = setupNetwork(networkName,orgDomain,peers);
+		if(res == false)
+			return false;
+	}
 
-	logger.log(deploy_writer,"==================\n Starting to setup infra \n ==========================");
+	res = shell.exec("docker-compose -f docker-compose-cli.yaml up", (err, stdout, stderr) => {
+	    if (err) {
+	        // node couldn't execute the command
+	        console.log("Bringing up network failed!")
+	        logger.log('deployer',"Bringing up network failed!");
+	        logger.log('deployer',stderr);
+	        return false;
+	    }
+	    logger.log('deployer', "==================  Network up and running!!  ========================== ");
+	    return true;
+	});
 
-	setupNetwork(networkName,orgDomain,peers);
+	if(res == false)
+		return false;
 
 	endorsers = "";
 	
 	var first = true;
+	
+	for(var iter=0; iter<peers.length; iter++){
 
-	for(var peer in peers){
-
-		createJoinChannel(peer,networkName,orgDomain,first);
-		logger.log(deploy_writer,"==================\n Channel for " + peer + " created and joined\n ==========================\n");
+		res = createJoinChannel(peers[iter],networkName,orgDomain,first);
+		if(res == false)
+			return false;
+		logger.log('deployer',"==================  Channel for " + peers[iter] + " created and joined  ========================== ");
 
 		if(first == true){
 			first = false;
@@ -105,21 +209,44 @@ function deploy(networkName,orgDomain,peers){
 		else{
 			endorsers += ",";
 		}
-		endorsers += "'" + peer + "MSP.peer'";
+		endorsers += "'" + peers[iter] + "MSP.peer'";
 
 	}
 
-	for(var peer in peers){
+	for(var iter=0; iter<peers.length; iter++){
 
-		shell.exec("docker exec" + peer + "_cli bash /bin/sh -c 'peer chaincode install -n mycc -v 1.0 -p github.com/chaincode/" + networkName + "'");
-		logger.log(deploy_writer,"==================\n " + peer + " installed chaincode\n ==========================\n");
-		
+		res = shell.exec("docker exec" + peers[iter] + "_cli bash /bin/sh -c 'peer chaincode install -n mycc -v 1.0 -p github.com/chaincode/" + networkName + "'", (err, stdout, stderr) => {
+		    if (err) {
+		        // node couldn't execute the command
+		        console.log("Installing chaincode failed on " + peers[iter])
+		        logger.log("Installing chaincode failed on " + peers[iter]);
+		        logger.log('deployer',stderr);
+		        return false;
+		    }
+		    logger.log('deployer', "==================" + peers[iter] + " installed chaincode  ========================== ");
+		    return true;
+		});		
+		if(res == false)
+			return false;
 	}
 
-	shell.exec("docker exec" + peers[0] + "_cli bash /bin/sh -c 'peer chaincode instantiate -o orderer." + orgDomain + ":7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/" + orgDomain + "/orderers/orderer." + orgDomain + "/msp/tlscacerts/tlsca." + orgDomain + "-cert.pem -C " + channelName + " -n mycc -v 1.0 -c '{\"Args\":[\"init\"]}' -P \"OR (" + endorsers + ")\"'");
-	logger.log(deploy_writer,"==================\n Chaincode instantiated!\n ==========================\n");
-
+	res = shell.exec("docker exec" + peers[0] + "_cli bash /bin/sh -c 'peer chaincode instantiate -o orderer." + orgDomain + ":7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/" + orgDomain + "/orderers/orderer." + orgDomain + "/msp/tlscacerts/tlsca." + orgDomain + "-cert.pem -C " + channelName + " -n mycc -v 1.0 -c '{\"Args\":[\"init\"]}' -P \"OR (" + endorsers + ")\"'", (err, stdout, stderr) => {
+	    if (err) {
+	        // node couldn't execute the command
+	        console.log("Instantiating chaincode failed")
+	        logger.log("Instantiating chaincode failed");
+	        logger.log('deployer',stderr);
+	        return false;
+	    }
+	    logger.log('deployer',"==================  Chaincode instantiated!  ========================== ");
+	    return true;
+	});
+	if(res == false)
+		return false;
 }
 
 
-deploy("pizza","demo0625.com",['restaurant','customer','deliverer']);
+deploy("pizza","demo0625.com",['restaurant','customer','deliverer'],"new");
+
+//up
+//down
--- src/Logger/logger.js
+++ src/Logger/logger.js
@@ -1,71 +1,36 @@
 var fs = require('fs');
 
-var date = new Date();
+var logPath, file;
 
-var logPath, deployer_writer, translator_writer, invoker_writer;
+exports.init = function(domain,networkName){
 
-/*module.exports = function init(){
-
-	if (!fs.existsSync('../../logs')) {
-	    fs.mkdirSync('../../logs');
+	if (!fs.existsSync('../../../logs')) {
+	    fs.mkdirSync('../../../logs');
 	}
 
-	if (!fs.existsSync('../../logs/'+domain)) {
-	    fs.mkdirSync('../../logs/'+domain);
+	if (!fs.existsSync('../../../logs'+domain)) {
+	    fs.mkdirSync('../../../logs'+domain);
 	}
 
-	if (!fs.existsSync('../../logs/'+domain+'/'+networkName)) {
-	    fs.mkdirSync('../../logs/'+domain+'/'+networkName);
+	if (!fs.existsSync('../../../logs'+domain+'/'+networkName)) {
+	    fs.mkdirSync('../../../logs'+domain+'/'+networkName);
 	}
 
-	logPath = '../../logs/' + networkName + '/';
-
-	deployer_writer = fs.createWriteStream(logPath + 'deployer_log.txt');
-
-	translator_writer = fs.createWriteStream(logPath + 'translator_log.txt');
-
-	invoker_writer = fs.createWriteStream(logPath + 'invoker_log.txt');
-
-	return translator_writer;
-}*/
-
-exports.getWriter = function(type,domain,networkName){
-
-	if (!fs.existsSync('../../logs')) {
-	    fs.mkdirSync('../../logs');
-	}
-
-	if (!fs.existsSync('../../logs/'+domain)) {
-	    fs.mkdirSync('../../logs/'+domain);
-	}
-
-	if (!fs.existsSync('../../logs/'+domain+'/'+networkName)) {
-	    fs.mkdirSync('../../logs/'+domain+'/'+networkName);
-	}
-
-	logPath = '../../logs/' + domain + '/' + networkName + '/';
-
-	deployer_writer = fs.createWriteStream(logPath + 'deployer_log.txt');
-
-	translator_writer = fs.createWriteStream(logPath + 'translator_log.txt');
-
-	invoker_writer = fs.createWriteStream(logPath + 'invoker_log.txt');
-
-	console.log("type is ::::::::::::::::::::"  + type);
-
-	if(type == 'deployer')
-		return deployer_writer;		
-	else if(type == 'invoker')
-		return invoker_writer;
+	logPath = '../../../logs' + domain + '/' + networkName + '/';
+	
 }
 
-exports.log = function(writer,text){
-	if(writer == null){
-		console.log("Failed to log!");
-		return;
-	}
-	var current_hour = date.getHours();
+exports.log = function(type,text){
 
-	writer.write(current_hour + ":: " +  text);
-	console.log("written!");
+	if(type == 'deployer')
+		file = logPath + 'deployer_log.txt';
+	else if(type == 'invoker')
+		file = logPath + 'invoker_log.txt';		
+	else
+		file = logPath + 'translator_log.txt';
+	fs.appendFile(file, new Date() + ":: " +  text + "\n", function (err) {
+		if (err) 
+			throw err;
+		console.log('written!');
+	});
 }
