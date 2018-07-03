3d18fc4b267bfb33d406010c28eec0bdd87f2536
Aditya Kamble <akamble@andrew.cmu.edu>
2018-07-03 17:07:53 -0400

    better unique id
    Also changed uniqle to unique

--- src/Server/REST.js
+++ src/Server/REST.js
@@ -7,6 +7,9 @@
 var mysql   = require("mysql");
 var fs = require("fs");
 var crypto = require('crypto');
+
+const uniqueString = require('unique-string');
+
 var parse = require("../Translator/parser.js");
 
 const getPortSync = require('get-port-sync');
@@ -39,7 +42,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
             if (err) throw err;
             console.log("Query all networks");
             res.render('index',{
-                                uniqle_id: "N/A",
+                                unique_id: "N/A",
                                 all_networks: result,
                                 translate_results: "N/A",
                                 compile_results: "N/A",
@@ -93,9 +96,9 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
         };
         console.log(receive);
 
-        var uniqle_id = crypto.createHash('md5').update(receive.xmlModel).digest('hex').substring(0, 5);
-        console.log("uniqle_id created: " + uniqle_id); 
-        filename = "tmp/" + uniqle_id + ".bpmn";
+        var unique_id = uniqueString();
+        console.log("unique_id created: " + unique_id); 
+        filename = "tmp/" + unique_id + ".bpmn";
 
         fs.writeFile(filename, receive.xmlModel, function (err) {
             if (err) {
@@ -103,12 +106,12 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
             }
 
 
-            num_peers = parse(filename,uniqle_id);
+            num_peers = parse(filename,unique_id);
 
             translate_results = fs.readFileSync(filename);
 
-            query = "INSERT INTO bpmn (uniqle_id, status) VALUES (?,?)";
-            table = [uniqle_id,0];
+            query = "INSERT INTO bpmn (unique_id, status) VALUES (?,?)";
+            table = [unique_id,0];
             query = mysql.format(query,table);
             connection.query(query, function (err, result) {
                 if (err) throw err;
@@ -122,7 +125,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
                 console.log("Query all networks");
                 // send response
                 res.render('index',{
-                                uniqle_id: uniqle_id,
+                                unique_id: unique_id,
                                 all_networks: result,
                                 translate_results: translate_results,
                                 compile_results: "N/A",
@@ -140,8 +143,8 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     /*
     POST format
     {
-        // The only uniqle_id
-        "uniqle_id": 
+        // The only unique_id
+        "unique_id": 
         // The chaincode
         "chaincode":     
     }
@@ -150,13 +153,13 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
         console.log("Deploying Smart Contract" );
         
         receive = {
-          uniqle_id:req.body.uniqle_id,
+          unique_id:req.body.unique_id,
           chaincode:req.body.chaincode
         };
         console.log(receive);
-        filename = "../../out/" + receive.uniqle_id + "/chaincode/chaincode.go";
+        filename = "../../out/" + receive.unique_id + "/chaincode/chaincode.go";
 
-        // save in out/uniqle_id/chaincode/*.go
+        // save in out/unique_id/chaincode/*.go
         fs.writeFile(filename, receive.chaincode, function (err) {
             if (err) {
                 console.log(err);
@@ -171,7 +174,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
                 console.log("Query all networks");
                 // send response
                 res.render('index',{
-                                uniqle_id: receive.uniqle_id,
+                                unique_id: receive.unique_id,
                                 all_networks: result,
                                 translate_results: "translate_results",
 
@@ -190,8 +193,8 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     /*
     POST format
     {
-        // The only uniqle_id
-        "uniqle_id": 
+        // The only unique_id
+        "unique_id": 
         // The chaincode
         "chaincode":     
     }
@@ -204,14 +207,14 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
         }
 
         receive = {
-          uniqle_id:req.body.uniqle_id,
+          unique_id:req.body.unique_id,
           chaincode:req.body.chaincode
         };
         console.log(receive);
  
         var status;
-        query = "SELECT * FROM bpmn WHERE uniqle_id=?";
-        table = [receive.uniqle_id];
+        query = "SELECT * FROM bpmn WHERE unique_id=?";
+        table = [receive.unique_id];
         query = mysql.format(query,table);
         connection.query(query, function (err, result) {
             if (err) throw err;
@@ -219,11 +222,11 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
             console.log(result[0].status);
             status = result[0].status;
         });
-        // parameters: uniqle_id and status
-        status = deploy(receive.uniqle_id,status,ports);
+        // parameters: unique_id and status
+        status = deploy(receive.unique_id,status,ports);
 
-        query = "UPDATE bpmn SET status = ? WHERE uniqle_id = ?";
-        table = [status,receive.uniqle_id];
+        query = "UPDATE bpmn SET status = ? WHERE unique_id = ?";
+        table = [status,receive.unique_id];
         query = mysql.format(query,table);
         connection.query(query, function (err, result) {
             if (err) throw err;
@@ -236,7 +239,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
             console.log("Query all networks");
             // send response
             res.render('index',{
-                            uniqle_id: receive.uniqle_id,
+                            unique_id: receive.unique_id,
                             all_networks: result,
                             translate_results: "N/A",
                             compile_results: "N/A",
@@ -255,8 +258,8 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     /*
     POST format
     {
-        // The only uniqle_id
-        "uniqle_id": 
+        // The only unique_id
+        "unique_id": 
         // The chaincode
         "function_name":
         // The parameters, a list of parameters
@@ -267,14 +270,14 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
         console.log("Invoking Smart Contract: function " + req.body.function_name);
         
         receive = {
-          uniqle_id:req.body.uniqle_id,
+          unique_id:req.body.unique_id,
           function_name:req.body.function_name,
           parameters:req.body.parameters
         };
 
         console.log(receive);
         
-        invoke_results = invoke(receive.uniqle_id, function_name, parameters);     
+        invoke_results = invoke(receive.unique_id, function_name, parameters);     
 
         query = "SELECT * FROM bpmn";
         connection.query(query, function (err, result) {
@@ -282,7 +285,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
             console.log("Query all networks");
             // send response
             res.render('index',{
-                            uniqle_id: receive.uniqle_id,
+                            unique_id: receive.unique_id,
                             all_networks: result,
                             translate_results: "N/A",
                             compile_results: "N/A",
