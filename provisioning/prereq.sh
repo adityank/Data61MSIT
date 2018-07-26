bdd6da2c9d3d1615ff6767c4eca0ac4494f9c58f
Aditya Kamble <akamble@andrew.cmu.edu>
2018-07-26 16:21:35 -0400

    tested

--- provisioning/prereq.sh
+++ provisioning/prereq.sh
@@ -9,11 +9,10 @@ sudo yum install docker -y -qq
 sudo usermod -a -G docker ec2-user
 curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
-service docker start
+sudo service docker start
 sudo chkconfig docker on
 
 # -------------------------install Python (if not python 2.7 by default)-----------
-#yum install python -y -qq (already installed on ami)
 sudo yum -y install gcc
 
 # ---------------------install and setup go 1.9 (PATH & stuff)---------------------
@@ -25,8 +24,6 @@ mkdir $HOME/work
 export GOPATH=$HOME/work
 echo 'export GOPATH=$HOME/work' >> ~/.bash_profile
 
-go version
-
 # ------------------------install binaries and samples------------------------
 curl -sSL http://bit.ly/2ysbOFE | sudo bash -s 1.1.0 1.1.0 0.4.6
 
@@ -34,7 +31,6 @@ curl -sSL http://bit.ly/2ysbOFE | sudo bash -s 1.1.0 1.1.0 0.4.6
 go get -u github.com/hyperledger/fabric/core/chaincode/shim
 cd ~/work/src/github.com/hyperledger/fabric/
 git checkout v1.1.0
-go build
 
 # -----------------------install nodejs & npm-------------------
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
@@ -48,7 +44,6 @@ sudo yum -y localinstall mysql57-community-release-el7-8.noarch.rpm
 sudo yum -y install mysql-community-server
 sudo service mysqld start
 
-mysql --user=root --password="$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print substr($0, 92);}')"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Unchained#1';" | mysql --user=root --password="$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print substr($0, 92);}')" ""  --connect-expired-password
 echo "CREATE DATABASE bpmn;" | mysql --user=root --password="Unchained#1"
 
@@ -79,3 +74,7 @@ npm install -g strip-ansi
 
 export NODE_PATH=~/.nvm/versions/node/v8.9.4/lib/node_modules
 echo 'export NODE_PATH=~/.nvm/versions/node/v8.9.4/lib/node_modules' >> ~/.bash_profile
+
+sudo tar -xvf ~/aws_project/Data61MSIT.tar
+cd ~/aws_project/Data61MSIT/src/Server
+node server.js
