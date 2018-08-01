bf5fc42432ca25349a3fe06cd1b73f54373f88fe
Aditya Kamble <akamble@andrew.cmu.edu>
2018-08-01 11:54:55 -0400

    delete testXMLParser.js

--- testXMLParser.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var parse = require('xml-parser');
-var xml = fs.readFileSync('pizza.bpmn', 'utf8');
-var inspect = require('util').inspect;
- 
-var obj = parse(xml);
-console.log(inspect(obj, { colors: true, depth: Infinity }));
