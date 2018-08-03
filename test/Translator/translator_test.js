9a9da4e1e4dc7e9402072f152f243c548e4c054f
Leo Zhou <leoaction@gmail.com>
2018-08-03 16:52:48 -0400

    test case added

--- test/Translator/translator_test.js
+++ test/Translator/translator_test.js
@@ -14,12 +14,48 @@ console.log(result.num_peers);
 
 var parser = require('../../src/Translator/parser.js');
 var expect = require('chai').expect;
-var path = require('path'),
-    bpmn_root = path.join(__dirname, '../../bpmn_examples/');
-describe('Test name 1', function() {
-  it('description 1', function() {
-  	var translation_result = parser.parse_by_file(bpmn_root+'andgate.bpmn', 'test_andgate');
-  	console.log(translation_result.errors);
-    expect(translation_result.errors).to.be.equal(null);
-  });
+var path = require('path');
+var bpmn_root = path.join(__dirname, '../../bpmn_examples/');
+
+describe('Correct Translation', function() {
+	it('Parallel Gateway', function() {
+		var translation_result = parser.parse_by_file(bpmn_root+'andgate.bpmn', 'test_andgate');
+		console.log(translation_result.errors);
+		expect(translation_result.errors).to.be.equal(null);
+	});
+	it('Exclusive Gateway', function() {
+		var translation_result = parser.parse_by_file(bpmn_root+'nestedxorgood.bpmn', 'test_nestedxorgood');
+		console.log(translation_result.errors);
+		expect(translation_result.errors).to.be.equal(null);
+	});
+	it('Databased Exclusive Gateway', function() {
+		var translation_result = parser.parse_by_file(bpmn_root+'databased.bpmn', 'test_databased');
+		console.log(translation_result.errors);
+		expect(translation_result.errors).to.be.equal(null);
+	});
+	it('Pizza Example', function() {
+		var translation_result = parser.parse_by_file(bpmn_root+'pizza.bpmn', 'test_pizza');
+		console.log(translation_result.errors);
+		expect(translation_result.errors).to.be.equal(null);
+	});
 });
+/*
+describe('Expected Warning', function() {
+	it('Parallel Gateway', function() {
+		var translation_result = parser.parse_by_file(bpmn_root+'andgate.bpmn', 'test_andgate');
+		expect(translation_result.errors).to.be.equal(null);
+	});
+	it('Exclusive Gateway', function() {
+		var translation_result = parser.parse_by_file(bpmn_root+'nestedxorgood.bpmn', 'test_nestedxorgood');
+		expect(translation_result.errors).to.be.equal(null);
+	});
+	it('Databased Exclusive Gateway', function() {
+		var translation_result = parser.parse_by_file(bpmn_root+'databased.bpmn', 'test_databased');
+		expect(translation_result.errors).to.be.equal(null);
+	});
+	it('Pizza Example', function() {
+		var translation_result = parser.parse_by_file(bpmn_root+'pizza.bpmn', 'test_pizza');
+		expect(translation_result.errors).to.be.equal(null);
+	});
+});
+*/
